4b92901e8da2f413c924cbf1d8ac029d3a56cc5b
Dean Mantecca <74303258+DMatCity@users.noreply.github.com>
2022-02-07 11:41:21 +0000

    Added density curves to histogram and ouput of optimals Hyperparameters to support analysis

--- cmd-program.py
+++ cmd-program.py
@@ -1,14 +1,17 @@
 import sys
-import sklearn
+assert sys.version_info >= (3, 8)
+import os
 from sklearn.preprocessing import MinMaxScaler
-import tensorflow as tf
-from tensorflow import keras
-import keras_tuner as kt
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
+import tensorflow as tf
+assert tf.__version__ >= "2.6"
+from tensorflow import keras
+import keras_tuner as kt
 import numpy as np
 import pandas as pd
-import os
+from scipy import stats
+import datetime
 from functions import * 
 
 # Where to save the figures
@@ -18,7 +21,6 @@ os.makedirs(IMAGES_PATH, exist_ok=True)
 
 # Define some functions for callbacks
 early_stopping_cb = keras.callbacks.EarlyStopping(patience=5, restore_best_weights=True)
-#tensorboard_cb = keras.callbacks.TensorBoard(run_logdir)
 
 # Define n steps taken into account to predict t steps into the future  
 n_steps = 250
@@ -32,10 +34,19 @@ files = []
 for i in range(len(input_files)):
     files.append(input_files[i].partition('.')[0])
 
-# output list valuation set
+# output dataframe of normality test
+df_shapiro_norm = pd.DataFrame()
+df_shapiro_log = pd.DataFrame()
+
+# Output datframe for hyperparameters
+df_lstm_hyp = pd.DataFrame()
+df_cnn_lstm_hyp = pd.DataFrame()
+df_WaveNet_hyp = pd.DataFrame()
+
+# output dataframe valuation set
 df_output_val = pd.DataFrame(columns=['Linear Regr', 'LSTM', 'CNN-LSTM','WaveNet'], index=files)
 
-# output list test set
+# output dataframe test set
 df_output_test = pd.DataFrame(columns=['Linear Regr', 'LSTM', 'CNN-LSTM','WaveNet'], index=files)
 
 for i in range(len(input_files)):
@@ -50,9 +61,15 @@ for i in range(len(input_files)):
     db.describe().to_csv('{}/02_Output/Tables/Description of {}.csv'.format(sys.path[0], files[i]))
 
     # Create a histogram to inspect the distribution of the data
-    plt.hist(db, density=True)
-    save_fig('Histogram before Log Transformation of {}'.format(files[i]), IMAGES_PATH)
+    pd.DataFrame(db).plot(kind='density', legend=False, color='grey')
+    plt.hist(db, density=True, color='lightgrey')
+    save_fig('Histogram before Logarithmic Transformation of {}'.format(files[i]), IMAGES_PATH)
     plt.clf()
+
+    # Perform the Shapiro-Wilk test for Normality
+    shapiro_test = stats.shapiro(db)
+    df_shapiro_norm.loc[files[i], 'statistic'] = shapiro_test.statistic
+    df_shapiro_norm.loc[files[i], 'p value'] = shapiro_test.pvalue
 
     # Perfrom a  log transformation of the data to check if the closing prices look closer to a normal distribution
     db_log = np.log(db)
@@ -60,9 +77,19 @@ for i in range(len(input_files)):
     db_log.describe().to_csv('{}/02_Output/Tables/Description of log {}.csv'.format(sys.path[0], files[i]))
 
     # Create a histrogram to inspect the distribution of the log transformed data
-    plt.hist(db_log, density=True)
-    save_fig('Histogram after Log Transformation of {}'.format(files[i]), IMAGES_PATH)
+    pd.DataFrame(db_log).plot(kind='density', legend=False, color='grey')
+    plt.hist(db_log, density=True, color='lightgrey')
+    save_fig('Histogram after Logarithmic Transformation of {}'.format(files[i]), IMAGES_PATH)
     plt.clf()
+
+    # Perform the Shapiro-Wilk test for Normality
+    shapiro_test = stats.shapiro(db_log)
+    df_shapiro_log.loc[files[i], 'statistic'] = shapiro_test.statistic
+    df_shapiro_log.loc[files[i], 'p value'] = shapiro_test.pvalue
+
+    # Print out the results as a checkpoint
+    print(df_shapiro_norm)
+    print(df_shapiro_log)
 
     # Change the directory to where the plots are supposed to be saved
     os.makedirs(IMAGES_PATH, exist_ok=True)
@@ -161,27 +188,43 @@ for i in range(len(input_files)):
     tuner.search(X_train, Y_train, epochs=5, validation_data=(X_val, Y_val))
 
     # Get the best model
-    best_model = tuner.get_best_models()
-    best_model = best_model[0]
+    best_model = tuner.get_best_models(1)[0]
+
+    # Get the best hyperparameters
+    best_hyperparameters = tuner.get_best_hyperparameters(1)[0]
+    dict_hyperparameters = best_hyperparameters.values
+
+    for key, value in dict_hyperparameters.items():
+        df_lstm_hyp.loc[files[i], key] = value
+
+    # print out progress to check the work of the program
+    print(df_lstm_hyp)
 
     # Create a checkpoint to save the model
     checkpoint_cb = keras.callbacks.ModelCheckpoint('LSTM_{file}'.format(file=files[i]), save_best_only=True)
-    best_model.fit(X_train, Y_train, epochs=1000, validation_data=(X_val, Y_val), callbacks=[ early_stopping_cb, checkpoint_cb])
+    log_dir = '{path}/02_Output/Logs/LSTM_{file}'.format(path=sys.path[0],file=files[i]) + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_cb = keras.callbacks.TensorBoard(log_dir=log_dir)
+    learn_history = best_model.fit(X_train, Y_train, epochs=1000, validation_data=(X_val, Y_val), callbacks=[early_stopping_cb, checkpoint_cb, tensorboard_cb])
     
     # Check if already a model exists, and if so, load the model and compile it. Otherwise, save the current model.
     if os.path.exists('{path}/02_Output/Models/LSTM_{file}'.format(path=sys.path[0],file=files[i])) == True:
         best_model = keras.models.load_model('{path}/02_Output/Models/LSTM_{file}'.format(path=sys.path[0],file=files[i]), custom_objects={"MCDropout": MCDropout, "LastTimeStepMSE": last_time_step_mse}, compile=False)
         best_model.compile(loss="mse", optimizer='adam', metrics=[last_time_step_mse])
-        print('loading model...')
+        print('loading model LSTM {file}'.format(file=files[i]))
     else:
         best_model.save('{path}/02_Output/Models/LSTM_{file}'.format(path=sys.path[0],file=files[i]))
-        print('saving model...')
+        print('saving model LSTM {file}'.format(file=files[i]))
     
     # Extract the perfomance metrics on the model and store it in a database which will be exported later to a csv file.
     loss, metrics = best_model.evaluate(X_val, Y_val)
     df_output_val.loc[files[i],'LSTM'] = metrics
     loss, metrics = best_model.evaluate(X_test, Y_test)
     df_output_test.loc[files[i],'LSTM'] = metrics
+
+    # Plot learning curve
+    plot_learning_curves(learn_history.history["loss"], learn_history.history["val_loss"])
+    save_fig('LSTM Learning Curve {}'.format(files[i]),IMAGES_PATH)
+    plt.clf()
 
     # Print the model
     best_model.summary()
@@ -251,26 +294,45 @@ for i in range(len(input_files)):
     tuner.search(X_train, Y_train[:, 3::2], epochs=5, validation_data=(X_val, Y_val[:, 3::2]))
 
     # Get the top model
-    best_model = tuner.get_best_models()
-    best_model = best_model[0]
+    best_model = tuner.get_best_models(1)[0]
+
+    # Get the best hyperparameters
+    best_hyperparameters = tuner.get_best_hyperparameters(1)[0]
+    dict_hyperparameters = best_hyperparameters.values
+
+    for key, value in dict_hyperparameters.items():
+        df_cnn_lstm_hyp.loc[files[i], key] = value
+
+    # print out progress to check the work of the program
+    print(df_cnn_lstm_hyp)
 
     checkpoint_cb = keras.callbacks.ModelCheckpoint('CNN-LSTM_{file}'.format(file=files[i]), save_best_only=True)
-    best_model.fit(X_train, Y_train[:, 3::2], epochs=1000, validation_data=(X_val, Y_val[:, 3::2]), callbacks=[early_stopping_cb, checkpoint_cb])
+    log_dir = '{path}/02_Output/Logs/CNN-LSTM_{file}'.format(path=sys.path[0],file=files[i]) + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_cb = keras.callbacks.TensorBoard(log_dir=log_dir)
+    learn_history = best_model.fit(X_train, Y_train[:, 3::2], epochs=1000, validation_data=(X_val, Y_val[:, 3::2]), callbacks=[early_stopping_cb, checkpoint_cb, tensorboard_cb])
     
     if os.path.exists('{path}/02_Output/Models/CNN-LSTM_{file}'.format(path=sys.path[0],file=files[i])) == True:
         best_model = keras.models.load_model('{path}/02_Output/Models/CNN-LSTM_{file}'.format(path=sys.path[0], custom_objects={"MCDropout": MCDropout, "LastTimeStepMSE": last_time_step_mse}, file=files[i]), compile=False)
         best_model.compile(loss="mse", optimizer='adam', metrics=[last_time_step_mse])
-        print('loading model...')
+        print('loading model CNN-LSTM {file}'.format(file=files[i]))
     else:
         best_model.save('{path}/02_Output/Models/CNN-LSTM_{file}'.format(path=sys.path[0],file=files[i]))
-        print('saving model')
+        print('saving model CNN-LSTM {file}'.format(file=files[i]))
     
     loss, metrics = best_model.evaluate(X_val, Y_val[:, 3::2])
     df_output_val.loc[files[i],'CNN-LSTM'] = metrics
     loss, metrics = best_model.evaluate(X_test, Y_test[:, 3::2])
     df_output_test.loc[files[i],'CNN-LSTM'] = metrics
 
+    # Plot learning curve
+    plot_learning_curves(learn_history.history["loss"], learn_history.history["val_loss"])
+    save_fig('CNN-LSTM Learning Curve {}'.format(files[i]),IMAGES_PATH)
+    plt.clf()
+
     best_model.summary()
+
+    # Output the current database of the metrics
+    print(df_output_val, df_output_test)
    
     y_pred = best_model.predict(X_test[-1:,:,:])[:, -1][..., np.newaxis]
     y_plot = Y_test[-1:,-1:,:]
@@ -327,20 +389,31 @@ for i in range(len(input_files)):
     tuner.search_space_summary()
     tuner.search(X_train, Y_train, epochs=5, validation_data=(X_val, Y_val))
 
-    best_model = tuner.get_best_models()
-    best_model = best_model[0]
+    best_model = tuner.get_best_models(1)[0]
+
+    # Get the best hyperparameters
+    best_hyperparameters = tuner.get_best_hyperparameters(1)[0]
+    dict_hyperparameters = best_hyperparameters.values
+
+    for key, value in dict_hyperparameters.items():
+        df_WaveNet_hyp.loc[files[i], key] = value
+
+    # print out progress to check the work of the program
+    print(df_WaveNet_hyp)
 
     checkpoint_cb = keras.callbacks.ModelCheckpoint('WaveNets_{file}'.format(file=files[i]), save_best_only=True)
-    best_model.fit(X_train, Y_train, epochs=1000, validation_data=(X_val, Y_val), callbacks=[early_stopping_cb, checkpoint_cb])
+    log_dir = '{path}/02_Output/Logs/WaveNet_{file}'.format(path=sys.path[0],file=files[i]) + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_cb = keras.callbacks.TensorBoard(log_dir=log_dir)
+    learn_history = best_model.fit(X_train, Y_train, epochs=1000, validation_data=(X_val, Y_val), callbacks=[early_stopping_cb, checkpoint_cb, tensorboard_cb])
     
 
     if os.path.exists('{path}/02_Output/Models/WaveNets_{file}'.format(path=sys.path[0],file=files[i])) == True:
         best_model = keras.models.load_model('{path}/02_Output/Models/WaveNets_{file}'.format(path=sys.path[0],file=files[i]), compile=False)
         best_model.compile(loss="mse", optimizer='adam', metrics=[last_time_step_mse])
-        print('loading model...')
+        print('Loading model WaveNet {file}'.format(file=files[i]))
     else:
         best_model.save('{path}/02_Output/Models/WaveNets_{file}'.format(path=sys.path[0],file=files[i]))
-        print('saving model...') 
+        print('Saving model WaveNet {file}'.format(file=files[i])) 
 
     
     loss, metrics = best_model.evaluate(X_val, Y_val)
@@ -348,8 +421,15 @@ for i in range(len(input_files)):
     loss, metrics = best_model.evaluate(X_test, Y_test)
     df_output_test.loc[files[i],'WaveNet'] = metrics
 
+    # Plot learning curve
+    plot_learning_curves(learn_history.history["loss"], learn_history.history["val_loss"])
+    save_fig('WaveNet Learning Curve {}'.format(files[i]),IMAGES_PATH)
+    plt.clf()
 
     best_model.summary()
+
+    # Output the current database of the metrics
+    print(df_output_val, df_output_test)
     
     y_pred = best_model.predict(X_test[-1:,:,:])[:, -1][..., np.newaxis]
     y_plot = Y_test[-1:,-1:,:]
@@ -373,4 +453,10 @@ print(df_output_val, df_output_test)
 df_output_val.to_csv('{}/02_Output/Tables/Table_of_MSE_val.csv'.format(sys.path[0]))
 df_output_test.to_csv('{}/02_Output/Tables/Table_of_MSE_test.csv'.format(sys.path[0]))
 
+df_shapiro_norm.to_csv('{}/02_Output/Tables/Table of Shapiro.csv'.format(sys.path[0]))
+df_shapiro_log.to_csv('{}/02_Output/Tables/Table of Shapiro Log.csv'.format(sys.path[0]))
+
+df_lstm_hyp.to_csv('{}/02_Output/Tables/Table of Hyp LSTM.csv'.format(sys.path[0]))
+df_cnn_lstm_hyp.to_csv('{}/02_Output/Tables/Table of Hyp CNN-LSTM.csv'.format(sys.path[0]))
+df_WaveNet_hyp.to_csv('{}/02_Output/Tables/Table of Hyp WaveNet.csv'.format(sys.path[0]))
 
